d2f2103a8c684ecc82bdac550035ed8b6bba5b17
divrawat <divrawat2001@gmail>
2024-07-03 00:45:05 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,471 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.disastrouscatastrophicnecromancer.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Disastrous Catastrophic Necromancer</u></a> follows a powerful necromancer whose relentless pursuit of immortality unleashes devastating consequences. As he resurrects the dead, chaos ensues, threatening to destroy everything he holds dear. Now, he must confront the fallout of his actions before it's too late.`;
+
+export const MANGA_NAME = 'Disastrous Catastrophic Necromancer';
+export const MANGA_AUTHOR = 'Poisuga';
+export const MANGA_RELEASE = '2019';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Poisuga';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Fantasy Comedy Action';
+
+export const CHAPTER_PREFIX = 'disastrous-catastrophic-necromancer-chapter';
+export const IMAGE_PREFIX = 'images/disastrous-catastrophic-necromancer';
+export const NEXT_PREVIOUS_PREFIX = 'manga/disastrous-catastrophic-necromancer-chapter';
+export const URL_PREFIX = 'manga/disastrous-catastrophic-necromancer-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/disastrous-catastrophic-necromancer-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://i.ytimg.com/vi/XZXe2BaeNlA/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBwq5JsFlbvoD0TZZxhjoRahf_xlg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[75px] rounded-full mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Disastrous Catastrophic Necromancer';
+export const NavbarName = "DC Necromancer";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. The story follows a powerful necromancer whose relentless pursuit of immortality unleashes devastating consequences. As he resurrects the dead, chaos ensues, threatening to destroy everything he holds dear. Now, he must confront the fallout of his actions before it's too late.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "In the heart of a mystical realm, Disastrous Catastrophic Necromancer unfolds the tale of a once-respected necromancer whose insatiable thirst for power and immortality leads him down a treacherous path. Known for his mastery over life and death, he delves deeper into forbidden arcane arts, seeking to conquer the very essence of mortality itself."
+    },
+    {
+        id: 2,
+        content: "Driven by hubris and ambition, the necromancer begins to unravel the natural order. With each soul he calls back from the beyond, a ripple of chaos spreads through the land. Crops wither, rivers turn foul, and whispers of unrest echo through villages. Yet, blinded by his obsession, he presses on, oblivious to the mounting peril."
+    },
+    {
+        id: 3,
+        content: "As his experiments grow bolder, the consequences become more dire. Undead horrors roam the countryside, terrorizing innocent lives. Nature itself rebels against his interference, unleashing storms and earthquakes that threaten to tear the world asunder. The necromancer's allies turn against him, fearing the catastrophic implications of his actions."
+    },
+    {
+        id: 4,
+        content: "Haunted by guilt and remorse, the necromancer realizes too late the devastation he has wrought. Desperate to undo the damage, he must now confront the consequences of his reckless pursuit of power. With the fabric of reality fraying around him, he races against time to find a way to restore balance before the entire realm succumbs to darkness."
+    },
+    {
+        id: 5,
+        content: "Disastrous Catastrophic Necromancer is a gripping saga of magic, morality, and redemption, where one man's quest for immortality risks plunging the world into irreversible chaos. It is a cautionary tale of the dangers of unchecked ambition and the transformative power of remorse and responsibility in the face of overwhelming adversity."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 47
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "86",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "87",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "88",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "89",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "90",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "91",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "92",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "93",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "94",
+        "numImages": 17
+    }
+]
